98c278703b58c0530183814dd010c0ff7cca54ec
T-RAMAPILLAI <ramapillaithirumurugan@gmail.com>
2024-01-28 21:37:46 +0530

    updated

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all")
+request.send();
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    console.log(res);
+    for(var i =0;i<res.length;i++){
+        if(res[i].capital){
+            console.log(res[i].capital[0]+" "+res[i].area)
+        }
+        else{
+            console.log("Invalid capital"+res[i].area);
+        }
+
+    }
+}
